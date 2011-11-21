bef2ac0306021a16ca685173907d09aa5f508ac8
Nate Kidwell <nate@ludicast.com>
2011-11-21 14:57:46 -0500

    using .min file

--- test/angular-rails_test.rb
+++ test/angular-rails_test.rb
@@ -6,6 +6,9 @@ class AngularRailsTest < ActiveSupport::TestCase
   end
   
   test "angular.js is found as an asset" do
-    assert_not_nil @app.assets["angular"]
+    assert_not_nil @app.assets["angular.min"]
+  end
+	test "angular-helpers.coffee is found as an asset" do
+    assert_not_nil @app.assets["angular-helpers"]
   end
 end
