52b53f5d37e6a67426feba344172d62a221dc104
Nate Kidwell <nate@ludicast.com>
2011-12-17 12:38:16 -0500

    removed accidental test

--- test/angular-rails_test.rb
+++ test/angular-rails_test.rb
@@ -8,8 +8,8 @@ class AngularRailsTest < ActiveSupport::TestCase
   test "angular.js is found as an asset" do
     assert_not_nil @app.assets["angular.min"]
   end
-	test "angular-helpers.coffee is found as an asset" do
+	test "angle-up.js is found as an asset" do
     assert_not_nil @app.assets["angle-up"]
-		puts  @app.assets["angle-up"]
   end
+
 end
